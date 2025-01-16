7064843476b3284215c9334298a3a6e0ea5e832e
BellalaK <bellafreesh@gmail.com>
2025-01-16 13:58:30 -0600

    Refactor calculator functions to remove unused parameters and improve operator input handling

--- calc.py
+++ calc.py
@@ -1,22 +1,37 @@
-def add(x,y,a):
+def add(x,y):
     a = x+y
     return a
 
-def sub(x,y,a):
+def sub(x,y):
     a = x-y
     return a
 
-def mult(x,y,a):
+def mult(x,y):
     a = x*y
     return a
 
-def divide(x,y,a):
+def divide(x,y):
     a = x/y
     return a
 
 print("Welcome to Bella's Basic Calculator")
-x = float(input("Enter the first number: "))
 valid_ops = ["+", "-", "*", "/"]
-op = input("Enter an operator (+,-,*,/): ")
+
+x = float(input("Enter the first number: "))
+
+while True:
+    op = input("Enter an operator (+,-,*,/): ")
+    if op in valid_ops:
+        break
+    
 y = float(input("Enter the second number: "))
 
+if op == valid_ops[0]:
+    add(x,y)
+elif op == valid_ops[1]:
+    sub(x,y)
+elif op == valid_ops[2]:
+    mult(x,y)
+elif op == valid_ops[3]:
+    divide(x,y)
+    
